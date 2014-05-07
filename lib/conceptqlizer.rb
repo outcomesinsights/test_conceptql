552b18f99ee17f7270f595b4950a53591d0f782d
Ryan Duryea <aguynamedryan@gmail.com>
2014-05-07 14:27:11 -0700

    ConceptQLizer: add ordered_cql_query

--- lib/conceptqlizer.rb
+++ lib/conceptqlizer.rb
@@ -5,4 +5,26 @@ module ConceptQLizer
     conceptql_query = ConceptQL::Query.new(db, eval(File.read(file)))
     conceptql_query.query
   end
+
+  def ordered_cql_query(file, schema = nil)
+    cql_query(file, schema).order(*_columns)
+  end
+
+private
+  def _columns
+    %w(
+      person_id
+      condition_occurrence_id
+      death_id
+      drug_cost_id
+      drug_exposure_id
+      observation_id
+      payer_plan_period_id
+      procedure_cost_id
+      procedure_occurrence_id
+      visit_occurrence_id
+      start_date
+      end_date
+    ).map(&:to_sym)
+  end
 end

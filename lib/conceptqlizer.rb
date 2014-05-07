a9eec8a0e37ecc3a42e379a841c052f89fab4c58
Ryan Duryea <aguynamedryan@gmail.com>
2014-05-07 14:26:55 -0700

    ConceptQLizer: cql_query accepts an explicit schema to use

--- lib/conceptqlizer.rb
+++ lib/conceptqlizer.rb
@@ -1,6 +1,7 @@
 require 'conceptql/query'
 module ConceptQLizer
-  def cql_query(file)
+  def cql_query(file, schema = nil)
+    db.execute("SET search_path TO #{schema}") if schema
     conceptql_query = ConceptQL::Query.new(db, eval(File.read(file)))
     conceptql_query.query
   end

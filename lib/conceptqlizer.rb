6b9e85b1511b3f6e6ee38a05b3be19523820ccca
Ryan Duryea <aguynamedryan@gmail.com>
2015-05-08 10:48:38 -0700

    ConceptQLizer: show SQL on error, normalize columns being compared

--- lib/conceptqlizer.rb
+++ lib/conceptqlizer.rb
@@ -1,9 +1,15 @@
 require 'conceptql/query'
 module ConceptQLizer
   def cql_query(file, schema = nil)
-    db.execute("SET search_path TO #{schema}") if schema
-    conceptql_query = ConceptQL::Query.new(db, eval(File.read(file)))
-    conceptql_query.query
+    db.extension :error_sql
+    begin
+      db.execute("SET search_path TO #{schema}") if schema
+      conceptql_query = ConceptQL::Query.new(db, eval(File.read(file)))
+      conceptql_query.query.from_self.select(*_columns)
+    rescue Sequel::DatabaseError, PG::UndefinedColumn
+      puts $!.sql
+      raise $!
+    end
   end
 
   def ordered_cql_query(file, schema = nil)
@@ -11,30 +17,21 @@ module ConceptQLizer
   end
 
 private
-=begin
   def _columns
-    %w(
+    %i(
       person_id
-      condition_occurrence_id
-      death_id
-      drug_cost_id
-      drug_exposure_id
-      observation_id
-      payer_plan_period_id
-      procedure_cost_id
-      procedure_occurrence_id
-      visit_occurrence_id
+      criterion_id
+      criterion_type
       start_date
       end_date
-    ).map(&:to_sym)
+    ) +
+      [Sequel.as(:value_as_number, :value_as_number)] +
+    %i(
+      value_as_string
+      value_as_concept_id
+    )
   end
 
-  def _order_columns
-    _columns.map do |column|
-      Sequel.asc(column, nulls: :first)
-    end
-  end
-=end
   def _order_columns
     [:person_id, :criterion_type, :criterion_id, :start_date, :end_date]
   end
